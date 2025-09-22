e4cc2a07f724ca7a82af104a183e904b2625b538
Kirill <kirillkasparyants@ya.ru>
2025-09-22 18:38:25 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 nasm -f bin boot/loader.s -o bin/loader.bin &&
-i686-elf-gcc -c kernel/kernel.c -o bin/kernel.o -ffreestanding -m32 -I kernel/lib&&
+i686-elf-gcc -c kernel/kernel.c -o bin/kernel.o -ffreestanding -m32 -I kernel/lib &&
 i686-elf-ld -T boot/linker.ld -o bin/kernel.bin bin/kernel.o &&
 cat bin/loader.bin bin/kernel.bin > bin/os.bin &&
 cp bin/kernel.bin iso/boot/kernel.bin &&
